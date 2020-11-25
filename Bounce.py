743f975ec2d6204de12092f8443f2c508c5b9125
Bilal Yaqub <bilal._.yaqub@hotmail.com>
2020-11-25 02:01:23 +0000

    Initiated the basic window

---
+++ Bounce.py
@@ -0,0 +1,12 @@
+from tkinter import *
+import random
+import time
+
+window = Tk()
+window.title("Bounce")
+
+# Initiating a canvas as it is easier to do animation on a canvas
+canvas = Canvas(window, width=700, height=700).pack()
+
+
+window.mainloop()
