26fded1a331f75e71087c3078211517cb82916dd
Bilal Yaqub <bilal._.yaqub@hotmail.com>
2020-11-26 22:27:44 +0000

    Created Functionality for the ball to move in the X axis unpredictabely

--- Bounce.py
+++ Bounce.py
@@ -18,12 +18,12 @@ window.update()
 class Ball:
     def __init__(self, canvas):
         # So that these can be changed later
-        self.x = 0
-        self.y = -1
+        self.x = random.randint(-5, 5)  # Makes the game more unpredictable
+        self.y = -3
         self.canvas = canvas
 
         # Creating the ball
-        self.identity = self.canvas.create_oval(10, 10, 25, 25, fill="red")
+        self.identity = self.canvas.create_oval(5, 5, 25, 25, fill="red")
         self.canvas.move(self.identity, 350, 150)   # Moving the ball to the center of the screen
 
     # Defining a function to move the ball down
@@ -31,13 +31,18 @@ class Ball:
         self.canvas.move(self.identity, self.x, self.y)
 
         # Keeps track of the current position of the ball and stores in the variable position so we can create an if loop
-        position = self.canvas.coords(self.identity)
+        position = self.canvas.coords(self.identity)    # returns (x1, y1, x2, y2)
 
         # position[1] and [3] are the y coordinate of the ball; if the ball reaches the top it starts to come back down and if it reaches the bottom it comes back up
         if position[1] <= 1:
-            self.y = 1
-        if position[3] > 700:   # Removed border so now I can use the actual window height
-            self.y = -1
+            self.y = 3
+        if position[3] > 700:
+            self.y = -3
+        # position [0] & [2] are x coordinates; creating a while loop to prevent the ball from going out of the screen
+        if position[0] <= 0:
+            self.x = -self.x
+        if position[2] > 700:
+            self.x = -self.x
 
 
 ball = Ball(canvas)
