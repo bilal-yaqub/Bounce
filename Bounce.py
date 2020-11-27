6f39f04e391ff1af3d15af789c03924cb70b1af4
Bilal Yaqub <bilal._.yaqub@hotmail.com>
2020-11-27 00:29:59 +0000

    Added the paddle

--- Bounce.py
+++ Bounce.py
@@ -23,7 +23,7 @@ class Ball:
         self.canvas = canvas
 
         # Creating the ball
-        self.identity = self.canvas.create_oval(5, 5, 25, 25, fill="red")
+        self.identity = self.canvas.create_oval(10, 10, 25, 25, fill="red")
         self.canvas.move(self.identity, 350, 150)   # Moving the ball to the center of the screen
 
     # Defining a function to move the ball down
@@ -34,26 +34,45 @@ class Ball:
         position = self.canvas.coords(self.identity)    # returns (x1, y1, x2, y2)
 
         # position[1] and [3] are the y coordinate of the ball; if the ball reaches the top it starts to come back down and if it reaches the bottom it comes back up
-        if position[1] <= 1:
-            self.y = 3
-        if position[3] > 700:
-            self.y = -3
+        if position[1] <= 1 or position[3] > 700:
+            self.y = -self.y
         # position [0] & [2] are x coordinates; creating a while loop to prevent the ball from going out of the screen
-        if position[0] <= 0:
+        if position[0] <= 0 or position[2] > 700:
             self.x = -self.x
-        if position[2] > 700:
-            self.x = -self.x
+
+
+# Exactly the same as when defining the ball
+class Rectangle:
+    def __init__(self, canvas):
+        self.canvas = canvas
+        self.identity = self.canvas.create_rectangle(0, 0, 130, 30, fill='#264653')
+        self.canvas.move(self.identity, 300, 650)
+        self.x = 0  # We don't need the y value here
+
+    # When the user presses the left key the rectangles moves left as well
+    def move_left(self, event):
+        self.x = -3
+
+    # When the user presses the right key the rectangles moves right as well
+    def move_right(self, event):
+        self.x = 3
+
+    def decision(self):
+        # These are th key bindings
+        window.bind('<Left>', self.move_left)
+        window.bind('<Right>', self.move_right)
+        position = self.canvas.coords(self.identity)
+        self.canvas.move(self.identity, self.x, 0)
+        if position[0] <= 5 or position[2] >= 695:
+            self.x = 0  # Stops the paddle from moving out of the screen
 
 
 ball = Ball(canvas)
-
-canvas.create_rectangle(300, 650, 430, 680, fill='#264653')
-
-# window.bind('<left>', move_left)
-# window.bind('<right>', move_right)
+rectangle = Rectangle(canvas)
 
 # Initiating a while loop so thw window keeps updating and sleep is there so animations are smooth
 while True:
     ball.action()
+    rectangle.decision()
     window.update()
-    time.sleep(0.001)  # Allow for the animation to take place otherwise it would go very quick
+    time.sleep(0.01)  # Allow for the animation to take place otherwise it would go very quick
